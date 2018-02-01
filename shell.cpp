cee45a21885597ae744f5e73a34a93c9c286e36c
nipuntech10 <34623337+nipuntech10@users.noreply.github.com>
2018-02-01 13:18:05 +0530

    new

--- shell.cpp
+++ shell.cpp
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 #include<iostream>
 #define max 100
@@ -123,4 +119,4 @@ s1.display_elements();
 
 return 0;
 }
-//*******************************
+
